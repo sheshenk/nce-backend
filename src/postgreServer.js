c3e043c8d657120d9b0d408d55fa6e3dfaeca3cd
Shashank Acharya <acharya.s@icloud.com>
2022-06-07 17:21:32 +0800

    Update postgreServer.js

--- src/postgreServer.js
+++ src/postgreServer.js
@@ -2,7 +2,7 @@ import pg from "pg"
 const { Pool } = pg
 
 export const pool = new Pool({
-	user: 'adminnuswap',
+	user: 'admin',
 	host: 'localhost',
 	database: 'nuswap',
 	password: 'ncepassword',
