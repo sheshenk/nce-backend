cf0074c4daa76dfc2328dd37bf734864dd2d2a53
Shashank Acharya <acharya.s@icloud.com>
2022-06-07 17:21:06 +0800

    added market history module

--- src/apolloApplication.js
+++ src/apolloApplication.js
@@ -1,5 +1,6 @@
 import { createApplication } from "graphql-modules";
 import HTTPResponseModule from "../types/HTTPResponse.js";
+import MarketHistoryModule from "../types/MarketHistory.js";
 import UserModule from '../types/User.js'
 import WalletModule from "../types/Wallet.js";
 import WalletAssetModule from "../types/WalletAsset.js";
@@ -9,7 +10,8 @@ const apolloApplication = createApplication({
 		UserModule,
 		HTTPResponseModule,
 		WalletModule,
-		WalletAssetModule
+		WalletAssetModule,
+		MarketHistoryModule
 	]
 })
 
