010bf27576b5cc3a319bbec0c6b4d3fbdd3a22c3
Pitchappan P RM <46064869+Pitch2342@users.noreply.github.com>
2022-06-14 11:22:50 +0800

    Changes to queries to match new DB schema

--- db_functions/MarketHistory.js
+++ db_functions/MarketHistory.js
@@ -4,7 +4,7 @@ import { pool } from "../src/postgreServer.js";
 export const updateMarketHistory = async ({symbol, price, quantity}) => {
 	try {
 		const res = await pool.query(
-			`INSERT INTO market_history (symbol, open, close, high, low, volume) VALUES ($1, $2, $2, $2, $2, $3) ON CONFLICT (time, symbol) DO UPDATE SET high = GREATEST(market_history.high, $2), low = LEAST(market_history.low, $2), close = $2, volume = market_history.volume + $3 RETURNING *`,
+			`INSERT INTO market_history_$1 (open, close, high, low, volume, time) VALUES ($2, $2, $2, $2, $3, date_trunc('minute'::text, now())) ON CONFLICT (time) DO UPDATE SET high = GREATEST(market_history.high, $2), low = LEAST(market_history.low, $2), close = $2, volume = market_history.volume + $3 RETURNING time, open, close, high, low, volume`,
 			[symbol, price, quantity]
 		)
 		pubsub.publish('MH_UPDATED', { marketHistoryUpdated: res.rows[0] })
@@ -13,8 +13,8 @@ export const updateMarketHistory = async ({symbol, price, quantity}) => {
 }
 
 export const readMarketHistory = async (single, {symbol}) => {
-	var queryString = 'SELECT * FROM market_history ORDER BY time ASC'
-	if (symbol) queryString = `SELECT * FROM market_history WHERE symbol='${symbol}'`
+	var queryString = 'SELECT time, open, close, high, low, volume FROM market_history_btcusd ORDER BY time ASC'
+	if (symbol) queryString = `SELECT time, open, close, high, low, volume FROM market_history_${symbol}`
 	const res = await pool.query(queryString)
 	return res.rows.length ? single ? res.rows[0] : res.rows : null
 }
--- db_functions/OrderFilling.js
+++ db_functions/OrderFilling.js
@@ -4,7 +4,7 @@ import { pool } from "../src/postgreServer.js";
 export const createOrderFilling = async ({symbol, quantity, price}) => {
 	try {
 		const res = await pool.query(
-			`INSERT INTO order_fillings (symbol, quantity, price) VALUES ($1, $2, $3) RETURNING *`,
+			`INSERT INTO order_fillings_$1 (quantity, price) VALUES ($2, $3) RETURNING time, symbol, quantity, price`,
 			[symbol, quantity, price]
 		)
 		pubsub.publish('NEW_ORDER_FILLED', { newOrder: res.rows[0] })
@@ -13,8 +13,8 @@ export const createOrderFilling = async ({symbol, quantity, price}) => {
 }
 
 export const readOrderFillings = async (single, {symbol}) => {
-	var queryString = 'SELECT * FROM order_fillings ORDER BY time ASC'
-	if (symbol) queryString = `SELECT * FROM order_fillings WHERE symbol='${symbol}'`
+	var queryString = 'SELECT time, symbol, quantity, price FROM order_fillings_btcusd ORDER BY time ASC'
+	if (symbol) queryString = `SELECT time, symbol, quantity, price FROM order_fillings_${symbol}`
 	const res = await pool.query(queryString)
 	return res.rows.length ? single ? res.rows[0] : res.rows : null
 }
