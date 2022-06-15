83a9eeb77247db8d5364f72ac785487f5495a32e
Pitchappan P RM <46064869+Pitch2342@users.noreply.github.com>
2022-06-15 08:41:10 +0530

    closed order type definition (queries pending)

--- types/ClosedOrder.js
+++ types/ClosedOrder.js
@@ -1,1 +1,37 @@
+import { gql } from "apollo-server";
+import { createModule } from "graphql-modules";
+import { withFilter } from "graphql-subscriptions";
+import { pubsub } from "../src/apolloServer.js";
+// import { readClosedOrders } from "../db_functions/ClosedOrder.js";
+
+const ClosedOrderModule = createModule({
+	id: 'closed-order',
+	typeDefs: gql`
+		type ClosedOrder {
+			orderid: ID!
+            walletid: ID!
+            owner: String!
+            buySide: String!
+            quantity: Float!
+            symbol: String!
+            price: Float!
+            fillCost: Float!
+            fillPrice: Float!
+            createdAt: String!
+            filledAt: String!
+		}
+
+		type Query {
+			getClosedOrdersForSymbol(symbol: String!): [ClosedOrder!]
+		}
+
+	`,
+	resolvers: {
+		Query: {
+			getClosedOrdersForSymbol: (p, a, c) => readClosedOrders(a)
+		}
+	}
+})
+
+export default ClosedOrderModule
 
