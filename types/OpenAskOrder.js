7118e538cc209cc31d06d7bd1a1b1c84e049b7f2
Pitchappan P RM <46064869+Pitch2342@users.noreply.github.com>
2022-06-16 13:47:03 +0800

    Updated types for matching DB schema and new queries

--- types/OpenAskOrder.js
+++ types/OpenAskOrder.js
@@ -2,7 +2,7 @@ import { gql } from "apollo-server";
 import { createModule } from "graphql-modules";
 import { withFilter } from "graphql-subscriptions";
 import { pubsub } from "../src/apolloServer.js";
-import { readOpenAskOrders } from "../db_functions/OpenAskOrder.js";
+import { readOpenAskOrders, getOpenAskOrdersForSymbolAndUser } from "../db_functions/OpenAskOrder.js";
 
 const OpenAskOrderModule = createModule({
 	id: 'open-ask-order',
@@ -14,20 +14,22 @@ const OpenAskOrderModule = createModule({
             quantity: Float!
             symbol: String!
             price: Float!
-            openQuantity: Float!
-            fillCost: Float!
-            createdAt: String!
-            updatedAt: String!
+            openquantity: Float!
+            fillcost: Float!
+            createdat: String!
+            updatedat: String!
 		}
 
 		type Query {
 			getOpenAskOrdersForSymbol(symbol: String!): [OpenAskOrder!]
+			getOpenAskOrdersForSymbolAndUser(symbol: String!, owner: ID): [OpenAskOrder!]
 		}
 
 	`,
 	resolvers: {
 		Query: {
-			getOpenAskOrdersForSymbol: (p, a, c) => readOpenAskOrders(a)
+			getOpenAskOrdersForSymbol: (p, a, c) => readOpenAskOrders(a),
+			getOpenAskOrdersForSymbolAndUser: (p, a, c) => getOpenAskOrdersForSymbolAndUser(a)
 		}
 	}
 })
--- types/WalletAsset.js
+++ types/WalletAsset.js
@@ -1,18 +1,19 @@
 import { gql } from "apollo-server";
 import { createModule } from "graphql-modules";
-import { readWalletAsset, updateWalletAsset } from "../db_functions/WalletAsset.js";
+import { readWalletAsset, updateWalletAsset , getWalletAssetsWalletID} from "../db_functions/WalletAsset.js";
 
 const WalletAssetModule = createModule({
 	id: 'wallet-asset',
 	typeDefs: gql`
 		type WalletAsset {
-			walletid: ID!
+			walletid: String!
 			symbol: String!
 			amount: Float!
 		},
 		type Query {
 			getAllWalletAssets: [WalletAsset!]
 			getWalletAsset(walletid: ID!, symbol: String!): WalletAsset
+			getWalletAssetsWalletID(walletid: ID!): [WalletAsset]
 		},
 		type Mutation {
 			addWalletAsset(walletid: ID!, symbol: String!, amount: Float!): HTTPResponse
@@ -21,7 +22,9 @@ const WalletAssetModule = createModule({
 	resolvers: {
 		Query: {
 			getAllWalletAssets: (p, a, c) => readWalletAsset(false, {}),
-			getWalletAsset: (p, a, c) => readWalletAsset(true, a)
+			getWalletAsset: (p, a, c) => readWalletAsset(true, a),
+			getWalletAssetsWalletID: (p, a, c) => getWalletAssetsWalletID(a)
+
 		},
 		Mutation: {
 			addWalletAsset: (p, a, c) => updateWalletAsset(a)
