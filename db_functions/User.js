77474cf9b2ab8e6af2a08f83ce9f9660f699eade
BOTShiZJ <zhijian_shi@163.com>
2022-06-20 00:33:45 +0800

    add user wallet init

--- db_functions/User.js
+++ db_functions/User.js
@@ -2,11 +2,11 @@ import { pool } from "../src/postgreServer.js";
 import jwt from "jsonwebtoken"
 import { createWallet } from "./Wallet.js";
 
-export const createUser = async ({name, email, password, phone}) => {
+export const createUser = async ({ name, email, password, phone }) => {
 	try {
 		const res = await pool.query(
 			`INSERT INTO users (name, email, password, phone, balance) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-			[name, email, password, phone, 0.0]
+			[name, email, password, phone, 10000.0] // init balance of $10,000
 		)
 		const token = jwt.sign({
 			userid: res.rows[0].userid,
@@ -17,7 +17,7 @@ export const createUser = async ({name, email, password, phone}) => {
 	} catch (err) { return { status: 409, error: err.detail } }
 }
 
-export const readUser = async (single, {userid, email}) => {
+export const readUser = async (single, { userid, email }) => {
 	// console.log(userid)
 	// console.log(email)
 	var queryString = 'SELECT * FROM users ORDER BY userid DESC'
@@ -48,14 +48,14 @@ export const addBalance = async (params) => {
 		const res = await pool.query(
 			`UPDATE USERS SET BALANCE = BALANCE + ${params['amount']} WHERE userid=${params['userid']} RETURNING *`
 		)
-		return { status: 200, response: "ADDED BALANCE"}
-	} 
-	catch (err) { 
-		return { status: 409, error: "FAILED BALANCE ADD"} 
+		return { status: 200, response: "ADDED BALANCE" }
+	}
+	catch (err) {
+		return { status: 409, error: "FAILED BALANCE ADD" }
 	}
 }
 
-export const getUserBalance = async ({userid}) => {
+export const getUserBalance = async ({ userid }) => {
 	var queryString = `SELECT * FROM USERS WHERE userid=${userid}`
 	const res = await pool.query(queryString)
 	return res.rows.length ? res.rows[0].balance : 0
--- db_functions/Wallet.js
+++ db_functions/Wallet.js
@@ -1,16 +1,20 @@
 import { pool } from "../src/postgreServer.js";
 
-export const createWallet = async ({userid}) => {
+export const createWallet = async ({ userid }) => {
 	try {
 		const res = await pool.query(
 			`INSERT INTO wallet (owner) VALUES ($1) RETURNING *`,
 			[userid]
 		)
+		await pool.query(
+			`INSERT INTO wallet_assets (walletid, symbol, amount) VALUES ($1, $2, $3) RETURNING *`,
+			[userid, 'btcusd', 10.0]
+		)
 		return { status: 201, response: "New wallet created." }
 	} catch (err) { return { status: 409, error: err.detail } }
 }
 
-export const readWallet = async (single, {walletid, owner}) => {
+export const readWallet = async (single, { walletid, owner }) => {
 	var queryString = 'SELECT * FROM wallet ORDER BY walletid ASC'
 	if (owner) queryString = `SELECT * FROM wallet WHERE owner='${owner}'`
 	if (walletid) queryString = `SELECT * FROM wallet WHERE walletid='${walletid}'`
