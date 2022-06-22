4b495616f885f865e91ea5a8f6ebe1056423debf
BOTShiZJ <zhijian_shi@163.com>
2022-06-22 21:24:49 +0800

    open order subscribed

--- types/OpenAskOrder.js
+++ types/OpenAskOrder.js
@@ -1,8 +1,7 @@
 import { gql } from "apollo-server";
 import { createModule } from "graphql-modules";
-import { withFilter } from "graphql-subscriptions";
-import { pubsub } from "../src/apolloServer.js";
 import { readOpenAskOrders, getOpenAskOrdersForSymbolAndUser } from "../db_functions/OpenAskOrder.js";
+import { pubsub } from "../src/apolloServer.js";
 
 const OpenAskOrderModule = createModule({
 	id: 'open-ask-order',
@@ -25,11 +24,19 @@ const OpenAskOrderModule = createModule({
 			getOpenAskOrdersForSymbolAndUser(symbol: String!, owner: ID): [OpenAskOrder!]
 		}
 
+		type Subscription {
+			newAskOpenOrder(symbol:String!): [OpenAskOrder!]
+		}
 	`,
 	resolvers: {
 		Query: {
 			getOpenAskOrdersForSymbol: (p, a, c) => readOpenAskOrders(a),
 			getOpenAskOrdersForSymbolAndUser: (p, a, c) => getOpenAskOrdersForSymbolAndUser(a)
+		},
+		Subscription: {
+			newAskOpenOrder: {
+				subscribe: () => pubsub.asyncIterator(["UPDATE_ASK_ORDER"])
+			}
 		}
 	}
 })
--- types/OpenBidOrder.js
+++ types/OpenBidOrder.js
@@ -1,8 +1,7 @@
 import { gql } from "apollo-server";
 import { createModule } from "graphql-modules";
-import { withFilter } from "graphql-subscriptions";
-import { pubsub } from "../src/apolloServer.js";
 import { readOpenBidOrders, getOpenBidOrdersForSymbolAndUser } from "../db_functions/OpenBidOrder.js";
+import { pubsub } from "../src/apolloServer.js";
 
 const OpenBidOrderModule = createModule({
 	id: 'open-bid-order',
@@ -25,11 +24,20 @@ const OpenBidOrderModule = createModule({
 			getOpenBidOrdersForSymbolAndUser(symbol: String!, owner: ID): [OpenBidOrder!]
 		}
 
+		type Subscription {
+			newBidOpenOrder(symbol:String!): [OpenBidOrder!]
+		}
+
 	`,
 	resolvers: {
 		Query: {
 			getOpenBidOrdersForSymbol: (p, a, c) => readOpenBidOrders(a),
 			getOpenBidOrdersForSymbolAndUser: (p, a, c) => getOpenBidOrdersForSymbolAndUser(a)
+		},
+		Subscription: {
+			newBidOpenOrder: {
+				subscribe: () => pubsub.asyncIterator(["UPDATE_BID_ORDER"])
+			}
 		}
 	}
 })
